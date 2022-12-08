06dd131bfa33632b09ab756382c0a578b976babe
antoniobigas <antoniobigas@gmail.com>
2022-12-08 12:14:33 -0300

    Atualização Try Catch

--- exemplo-tratamento-excecoes/src/application/Program.java
+++ exemplo-tratamento-excecoes/src/application/Program.java
@@ -1,9 +1,18 @@
 package application;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
+        method1();
+        System.out.println("End of program");
+    }
+    public static void method1() {
+        System.out.println("***METHOD1 START***");
+        method2();
+        System.out.println("***METHOD1 END***");
+    }
+    public static void method2() {
+        System.out.println("***METHOD2 START***");
         Scanner sc = new Scanner(System.in);
         try {
             String[] vect = sc.nextLine().split(" ");
@@ -12,14 +21,13 @@ public class Program {
         }
         catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position!");
+            e.printStackTrace();
+            sc.next();
         }
         catch (InputMismatchException e) {
             System.out.println("Input error");
         }
-        System.out.println("End of program");
         sc.close();
+        System.out.println("***METHOD2 END***");
     }
-}
-
-
 }
