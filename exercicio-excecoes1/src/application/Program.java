cf4be6e0deb66de7ebe7db4f2ccfaea57f26638a
antoniobigas <antoniobigas@gmail.com>
2022-12-08 13:56:58 -0300

    Exercicio Try Catch + Exception

---
+++ exercicio-excecoes1/src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import entities.Account;
+import exceptions.BusinessException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Informe o número da conta");
+        System.out.println("Número: ");
+        int number = sc.nextInt();
+        System.out.println("Titular: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.println("Saldo inicial: ");
+        double balance = sc.nextDouble();
+        System.out.println("Limite de saque: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.println("Informe uma quantia para sacar: ");
+        double amount = sc.nextDouble();
+
+        try {
+            acc.withdraw(amount);
+            System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+        }
+        catch (BusinessException e) {
+            System.out.println(e.getMessage());
+        }
+        sc.close();
+
+
+
+
+    }
+
+}
