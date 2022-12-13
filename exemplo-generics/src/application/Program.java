eeeacd509a8352f62812fea5fd8c51909ac673c5
antoniobigas <antoniobigas@gmail.com>
2022-12-13 19:22:33 -0300

    Add CalculationSerivce + Products

--- exemplo-generics/src/application/Program.java
+++ exemplo-generics/src/application/Program.java
@@ -1,27 +1,40 @@
 package application;
 
-import services.PrintService;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-import java.util.Scanner;
+import entities.Product;
+import services.CalculationService;
 
 public class Program {
-    public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
 
-        PrintService<Integer> ps = new PrintService<>();
+    public static void main(String[] args) {
 
-        System.out.print("How many values? ");
-        int n = sc.nextInt();
+        Locale.setDefault(Locale.US);
 
-        for (int i = 0; i < n; i++) {
-            Integer value = sc.nextInt();
-            ps.addValue(value);
+        List<Product> list = new ArrayList<>();
+
+        String path = "C:\\temp\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            Product x = CalculationService.max(list);
+            System.out.println("Most expensive:");
+            System.out.println(x);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
-
-        ps.print();
-        Integer x = ps.first();
-        System.out.println("First: " + x);
-
-        sc.close();
     }
 }
